ba6105609f98637a467fb2420adf75b37966f6d0
vadimcimbalyk <vadymonua@gmail.com>
2022-03-30 12:41:51 +0300

    child

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,40 @@
 <template>
   <div class="container">
     <h1>Шпаргалка по Grid CSS</h1>
-    <Item title="flex-direction" :items="displayItem" />
-    <Item title="flex-direction" :items="justifyItem" />
+    <Item title="display" property-name="display"
+          :property-values="display"
+          :default-parent-style="{'grid-template': '50px 50px 50px / 50px 50px 50px'}"
+    />
+    <Item title="align-items" property-name="align-items"
+          :property-values="alignItems"
+          :default-parent-style="{display:'grid', 'grid-template': '50px 50px 50px / 50px 50px 50px'}"
+    />
+    <Item title="justify-items" property-name="justify-items"
+          :property-values="justifyItems"
+          :default-parent-style="{display:'grid', 'grid-template': '50px 50px 50px / 50px 50px 50px'}"
+    />
+    <Item title="justify-content" property-name="justify-content"
+          :property-values="justifyContent"
+          :default-parent-style="{display:'grid', 'grid-template': '50px 50px 50px / 50px 50px 50px'}"
+    />
+    <Item title="align-content" property-name="align-content"
+          :property-values="alignContent"
+          :default-parent-style="{display:'grid', 'grid-template': '50px 50px 50px / 50px 50px 50px'}"
+    />
+    <Item title="grid-auto-flow" property-name="grid-auto-flow"
+          :property-values="gridAutoFlow"
+          :default-parent-style="{display:'grid', 'grid-template': '50px 50px 50px / 50px 50px 50px'}"
+    />
+    <Item title="justify-self" property-name="justify-self"
+          :property-values="justifySelf"
+          :default-parent-style="{display:'grid', 'grid-template': '50px 50px 50px / 50px 50px 50px'}"
+          :style-dir="false"
+    />
+    <Item title="align-self" property-name="align-self"
+          :property-values="alignSelf"
+          :default-parent-style="{display:'grid', 'grid-template': '50px 50px 50px / 50px 50px 50px'}"
+          :style-dir="false"
+      />
   </div>
 </template>
 
@@ -15,14 +47,17 @@ export default {
   components: {Item},
   data() {
     return {
-      displayItem: [ 'grid','inline-grid','subgrid' ],
-      justifyItem: [ 'start','end','center','stretch' ],
-
+      display: ['grid', 'inline-grid'],
+      justifyItems: ['start', 'end', 'center', 'stretch'],
+      alignItems: ['start', 'end', 'center', 'stretch'],
+      justifyContent: ['start', 'end', 'center', 'stretch', 'space-between', 'space-around', 'space-evenly'],
+      alignContent: ['start', 'end', 'center', 'stretch', 'space-between', 'space-around', 'space-evenly'],
+      gridAutoFlow: ['row', 'column','dense'],
+      justifySelf: ['start', 'end','center','stretch'],
+      alignSelf: ['start', 'end','center','stretch'],
     }
-  },
-  methods:{
-
   }
+
 }
 </script>
 
@@ -30,14 +65,6 @@ export default {
 .container {
   max-width: 1170px;
   margin: 0 auto;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 
 h1 {
@@ -54,10 +81,32 @@ h2 {
   margin-bottom: 10px;
   padding: 8px 20px
 }
+.example {
+  height: 200px;
+  border: 2px solid #4fc3f7;
+  padding: 10px;
+}
+.example > div{
+  width: 100%;
+  height: 100%;
+}
 
+.items {
+  border: 2px solid #4fc3f7;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+}
 
-
-
+.child {
+  min-width: 2.5rem;
+  min-height: 2.5rem;
+  padding: 0.5rem;
+  background-color: #97dffa;
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+  border: 1px solid #fff;
+}
 
 
 </style>
--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,28 +1,24 @@
 <template>
   <div>
     <div class="container">
-      <div class="row">
+      <div class="row mb-3">
+        <h2>{{ title }}</h2>
         <div class="col">
-          <h2>{{ title }}</h2>
           <ul class="row d-flex m-0 p-0 text-center list-unstyled">
-            <li  class="col items p-1" v-for="item of items" :key="item" @click="changeDisplay(item)">
-              <p>{{item}}</p>
+            <li class="items p-1" v-for="value of propertyValues" :key="value" @click="changeValue(value)">
+              {{ value }}
             </li>
           </ul>
+          <div>
+            <p>.example</p>
+            <p style="white-space: pre">.parent:{{ editParentString }}</p>
+            <p style="white-space: pre">.child:{{ editChildString }}</p>
+          </div>
         </div>
         <div class="col">
           <div class="example">
-            there text
-            <div class="parent">
-              <div class="col child">1</div>
-              <div class="col child">2</div>
-              <div class="col child">3</div>
-              <div class="col child">4</div>
-              <div class="col child">5</div>
-              <div class="col child">6</div>
-              <div class="col child">7</div>
-              <div class="col child">8</div>
-              <div class="col child">9</div>
+            <div :style="parentStyle">
+              <div :style="childStyle" class="child" v-for="i of childrenQuantity" :key="i">{{ i }}</div>
             </div>
           </div>
         </div>
@@ -35,44 +31,90 @@
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Item",
+  data: function () {
+    return {
+      classParent: {},
+      activeValue: this.propertyValues[0]
+    }
+  },
   props: {
     title: {
       type: String
     },
-    items: {
+    propertyName: {
+      type: String
+    },
+    propertyValues: {
       type: Array
+    },
+    childrenQuantity: {
+      type: Number,
+      default: 9
+    },
+    defaultParentStyle: {
+      type: Object,
+      default: () => ({})
+    },
+    defaultChildStyle: {
+      type: Object,
+      default: () => ({})
+    },
+    styleDir: {
+      type: Boolean,
+      default: true
     }
   },
-  methods:{
-    changeDisplay(item){
-      document.querySelector('.parent').classList.toggle(item)
-    },
+  computed: {
+    parentStyle() {
+      if (this.styleDir) {
+        return {
+          ...this.defaultParentStyle,
+          [this.propertyName]: this.activeValue
+        }
+      }
+      return {
+        ...this.defaultParentStyle
+      }
 
+    },
+    editParentString() {
+        return JSON.stringify(this.parentStyle)
+            .replace(/"/gm, '')
+            .replace(/,/gm, ';\n')
+            .replace(/{/gm, '{\n')
+            .replace(/}/gm, '\n}')
+    },
+    editChildString(){
+      if(!this.styleDir){
+        return JSON.stringify(this.childStyle)
+            .replace(/"/gm, '')
+            .replace(/,/gm, ';\n')
+            .replace(/{/gm, '{\n')
+            .replace(/}/gm, '\n}')
+      }
+      return ''
+    },
+    childStyle() {
+      if (!this.styleDir) {
+        return {
+          ...this.defaultChildStyle,
+          [this.propertyName]: this.activeValue
+        }
+      }
+      return {
+        ...this.defaultChildStyle
+      }
+    }
+  },
+  methods: {
+    changeValue(value) {
+      this.activeValue = value
+    },
   }
 }
 </script>
 
 <style>
-.container {
-  max-width: 1170px;
-  margin: 0 auto;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1 {
-  text-align: center;
-}
-
-h2 {
-  text-align: start;
-}
 
 .item > div {
   border: 2px solid #4fc3f7;
@@ -80,59 +122,4 @@ h2 {
   margin-bottom: 10px;
   padding: 8px 20px
 }
-
-.example {
-  min-height: 155px;
-  border: 2px solid #4fc3f7;
-  padding: 10px;
-}
-
-.items{
-  border: 2px solid #4fc3f7;
-  margin-right: 10px;
-  margin-bottom: 10px;
-  cursor: pointer;
-}
-.parent {
-
-  grid-template:50px 50px 50px / 50px 50px 50px;
-  border: 1px dotted #a6a6a6;
-  height: 240px;
-}
-.child{
-  min-width: 2.5rem;
-  min-height: 2.5rem;
-  padding: 0.5rem;
-  background-color: #97dffa;
-  background-repeat: no-repeat;
-  background-position: 50% 50%;
-  border: 1px solid #fff;
-}
-.grid{
-  display: grid;
-}
-.subgrid{
-  display: grid;
-  grid-template-columns: subgrid;
-}
-.inline-grid{
-  display: inline-grid;
-}
-.start{
-  display: grid;
-  justify-items: start;
-}
-.end{
-  display: grid;
-  justify-items: end;
-}
-.center{
-  display: grid;
-  justify-items: center;
-}
-.stretch{
-  display: grid;
-  justify-items: stretch;
-}
-
 </style>
