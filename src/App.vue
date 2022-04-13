61b08dbd3f1fe0926c351b8293b2ad9def17d0a6
Tkachyshen Andrei <tkachyshen.a@gmail.com>
2022-04-13 15:44:49 +0300

    fix name page

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,13 @@
         <router-link to="/grid-hint">Grid шпаргалка</router-link>
       </div>
       <div class="col">
-        <router-link to="/flex-generator">Генератор FlexBox</router-link>
+        <router-link to="/flex-generator">Генератор FlexBox(v1)</router-link>
       </div>
       <div class="col">
         <router-link to="/flex-box">FlexBox шпаргалка</router-link>
       </div>
       <div class="col">
-        <router-link to="/VFlexExample">VFlexExample</router-link>
+        <router-link to="/VFlexExample">Генератор FlexBox(v2)</router-link>
       </div>
     </div>
     <div class="row">
